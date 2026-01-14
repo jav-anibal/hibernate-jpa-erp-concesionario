67b01716470c0bb53647ec2ac7e1bdccda44dc25
anibaL <a88anibal@gmail.com>
2026-01-14 12:24:53 +0100

    F3 -> prueba de conexion - gestorEntity

--- src/main/java/org/backend/Main.java
+++ src/main/java/org/backend/Main.java
@@ -1,8 +1,12 @@
 package org.backend;
 
 
+import org.backend.service.GestorEntityManager;
+
 public class Main {
     public static void main(String[] args) {
+
+        GestorEntityManager.iniciarConexion();
 
     }
 }
